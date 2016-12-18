67bb62061221d139b612cbcb30a9484f7dc7639a
canegron <canegron92@yahoo.com>
2016-12-18 15:24:29 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,1 @@
-<NAME>Television en Vivo</NAME><URL>https://kodipuertorico.000webhostapp.com/TV/canales.php</URL><ICON> </ICON><FANART> </FANART><DESC> </DESC>
+<NAME>Television en Vivo</NAME><URL>https://raw.githubusercontent.com/canegron/KodiPuertoRico/master/TV/canales.php</URL><ICON> </ICON><FANART> </FANART><DESC> </DESC>
